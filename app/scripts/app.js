ff8ae18eca18ce71db68792c1408874f7c23984d
Jhonatan Laguna <JhonLPdev@gmail.com>
2015-07-16 22:35:58 -0700

    Archivos base

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name reCodeMeApp
+ * @description
+ * # reCodeMeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('reCodeMeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reCodeMeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reCodeMeApp
+ */
+angular.module('reCodeMeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
