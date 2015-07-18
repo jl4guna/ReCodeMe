9d3f7968c115419b398b13643af154d3a0f900b3
Jhonatan Laguna <JhonLPdev@gmail.com>
2015-07-17 21:14:01 -0700

    Vista pricipal (main) terminada

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,28 @@
  * Controller of the reCodeMeApp
  */
 angular.module('reCodeMeApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $mdDialog) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.alert = '';
+    $scope.showConfirm = function(ev) {
+      // Appending dialog to document.body to cover sidenav in docs app
+      var confirm = $mdDialog.confirm()
+        .parent(angular.element(document.body))
+        .title('¿Estas Seguro?')
+        .content('Esta Accion elminara el item seleccionado')
+        .ariaLabel('Borrar Item')
+        .ok('Borrar')
+        .cancel('Mejor no')
+        .targetEvent(ev);
+        $mdDialog.show(confirm).then(function() {
+          console.log('Borrado');
+        }, function() {
+          console.log('No Borrado');
+        });
+    };
+
   });
