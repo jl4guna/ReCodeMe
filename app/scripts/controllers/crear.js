0fcbd9b7be6903853f5dab8072e0cea0dd0f9373
Jhonatan Laguna <JhonLPdev@gmail.com>
2015-08-08 17:36:26 -0700

    Controller de crear terminado

--- app/scripts/controllers/crear.js
+++ app/scripts/controllers/crear.js
@@ -8,12 +8,31 @@
  * Controller of the reCodeMeApp
  */
 angular.module('reCodeMeApp')
-  .controller('CrearCtrl', function ($scope) {
-    $scope.nota = {
-        title : '',
-        description : ''
-    };
-
+  .controller('CrearCtrl', function ($scope, $mdDialog) {
     $scope.tags = [];
 
+    $scope.nota = {
+        title : '',
+        description : '',
+        tags : $scope.tags,
+        code : $scope.code
+    };
+
+
+    $scope.showConfirm = function(ev) {
+      // Appending dialog to document.body to cover sidenav in docs app
+      var confirm = $mdDialog.confirm()
+        .parent(angular.element(document.body))
+        .title('¿Estas Seguro?')
+        .content('Esta Accion elminara el item seleccionado')
+        .ariaLabel('Borrar Item')
+        .ok('Borrar')
+        .cancel('Mejor no')
+        .targetEvent(ev);
+        $mdDialog.show(confirm).then(function() {
+          console.log('Borrado');
+        }, function() {
+          console.log('No Borrado');
+        });
+    };
   });
